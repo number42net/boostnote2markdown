61edadab15b6fc3571f22c9d6f1cf84998695da7
Martin Miedema <github@number42.net>
2021-04-08 17:04:29 +0200

    Added attachment support

--- convert.py
+++ convert.py
@@ -10,8 +10,9 @@
 import json
 import glob
 import os
-from pathlib import Path
 import warnings
+from pathlib import Path
+from shutil import copyfile
 
 # Disable deprication warnings, not much we can do to avoid those.
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -19,35 +20,46 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 # Find all .json files in the notes directory
 paths = glob.glob("notes/*.json")
 
+# Inventory attachments
+attachments = glob.glob("attachments/*")
+
+def write_file(folder, title, content):
+    # Unescape the content
+    content = data['content'].encode().decode('unicode-escape')
+
+    if not os.path.isdir(path.parent):
+        os.makedirs(path.parent)
+
+    # Check if any attachments are referenced:
+    for attachment in attachments:
+        # Check if the attachment name is referenced with ](attachment_name)
+        if f']({attachment.replace("attachments/", "")})' in content:
+            # Copy the file and remove the atachment from the list.
+            copyfile(f'{attachment}', f'{path.parent}/{attachment.replace("attachments/","")}')
+            attachments.remove(attachment)
+    # Write the content to the file
+    with open(path.__str__().lstrip("/"), 'w') as output_file:
+        output_file.write(content)
+
 untitled_counter = 1
 for file in paths:
     with open(file) as json_file:
         data = json.load(json_file)
 
     # Create the directory and file name using the title if it has one.
-    if data['title']:
+    if data['title'] and data['content']:
         path = Path(f"{data['folderPathname'].strip('/')}/{data['title']}.md")
-        # Unescape the content
-        content = data['content'].encode().decode('unicode-escape')
-
-        if not os.path.isdir(path.parent):
-            os.makedirs(path.parent)
-
-        # Write the content to the file
-        with open(path.__str__().lstrip("/"), 'w') as output_file:
-            output_file.write(content)
+        write_file(path, data['title'], data['content'])
 
     # If there is content, but no title, create a untitled file using the counter
     elif data['content']:
         path = Path(f"{data['folderPathname'].strip('/')}/untitled_{untitled_counter:03d}.md")
-        # Unescape the content
-
-        content = data['content'].encode().decode('unicode-escape')
-
-        if not os.path.isdir(path.parent):
-            os.makedirs(path.parent)
-
-        # Write the content to the file
-        with open(path.__str__().lstrip("/"), 'w') as output_file:
-            output_file.write(content)
+        write_file(path, data['title'], data['content'])
         untitled_counter = untitled_counter + 1
+
+    else:
+        print (f"File '{file}' has no content")
+
+# Output any attachments that were not used:
+for file in attachments:
+    print (f"No note found for attachment: {file}")
